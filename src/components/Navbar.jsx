4c5864401dd32acd1b506ef75c837cd19eb1ed0e
Q-SRV <sumathrengasami@gmail.com>
2025-12-06 21:02:21 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+// src/components/Navbar.jsx
+import { NavLink, Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+
+const navLinkClass = ({ isActive }) =>
+  "nav-link " + (isActive ? "nav-link-active" : "");
+
+function Navbar() {
+  return (
+    <header className="navbar">
+      <div className="navbar-inner">
+
+        {/* LEFT LOGO (LINK TO HOME) */}
+        <Link to="/" className="logo">
+          <img src={logo} alt="Stamatics Logo" className="logo-image" />
+          <div className="logo-text">
+            <span className="logo-title">Stamatics</span><br />
+            <span className="logo-subtitle">IIT Kanpur</span>
+          </div>
+        </Link>
+
+        {/* RIGHT NAVIGATION */}
+        <nav className="nav-links top-right-nav">
+
+          {/* ABOUT US (scrolls within Home page) */}
+          <a href="/#about" className="nav-link">
+            About Us
+          </a>
+
+          {/* COMPETITIONS DROPDOWN */}
+          <div className="dropdown">
+            <span className="dropdown-label">Competitions ▾</span>
+
+            <div className="dropdown-menu">
+              <a href="/#integration" className="dropdown-item">
+                Integration Bee
+              </a>
+
+              {/* Mathemania has its own page */}
+              <NavLink to="/mathemania" className="dropdown-item">
+                Mathemania
+              </NavLink>
+
+              <a href="/#participants" className="dropdown-item">
+                Participants
+              </a>
+            </div>
+          </div>
+
+          {/* BLOGS */}
+          <NavLink to="/blogs" className={navLinkClass}>
+            Blogs
+          </NavLink>
+
+          {/* OUR TEAM */}
+          <NavLink to="/team" className={navLinkClass}>
+            Our Team
+          </NavLink>
+
+          {/* CONTACT */}
+          <NavLink to="/contact" className={navLinkClass}>
+            Contact
+          </NavLink>
+        </nav>
+
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
