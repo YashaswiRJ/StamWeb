1594f7315aae3a6a46f837e17e140103207b18fb
Verma <verma@Yashaswis-MacBook-Air.local>
2026-01-15 11:07:15 +0530

    Forms rectified env var

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // Central configuration file
 // Replace this URL with your latest deployed Web App URL
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwIRk4Qu7Q7YC7dJXuULLz1zAjaawMspe-AWCzHy7AAAXboEg32oP2YYGEbDGDpu23i/exec"
+export const GOOGLE_SCRIPT_URL = import.meta.env.VITE_API_URL;
